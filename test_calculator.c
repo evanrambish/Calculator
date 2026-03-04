f455cf648c789f880cc5be93bb9eb3508cfddaeb
Evan Plascencia-Rambish <evanp@cpp.edu>
2026-03-03 17:49:46 -0800

    Add overflow and underflow tests for add()

--- test_calculator.c
+++ test_calculator.c
@@ -1,5 +1,6 @@
 #include "unity.h"
 #include "calculator.h"
+#include <limits.h>
 // Optional but needs to be defined(runs BEFORE each test)
 void setUp(void) {
 // Initialize any resources needed for your tests here
@@ -27,6 +28,15 @@ void test_add_zero(void) {
     TEST_ASSERT_EQUAL(10, add(10, 0));
     TEST_ASSERT_EQUAL(0, add(0, 0));
 }
+void test_add_overflow(void) {
+    int result = add(INT_MAX, 1);
+    TEST_ASSERT_TRUE(result < 0);
+}
+
+void test_add_underflow(void) {
+    int result = add(INT_MIN, -1);
+    TEST_ASSERT_TRUE(result > 0);
+}
 int main(void) {
     UNITY_BEGIN();
 
@@ -34,6 +44,8 @@ int main(void) {
     RUN_TEST(test_add_positive_and_negative_numbers);
     RUN_TEST(test_add_negative_numbers);
     RUN_TEST(test_add_zero);
+    RUN_TEST(test_add_overflow);
+    RUN_TEST(test_add_underflow);
 
     return UNITY_END();
 }
