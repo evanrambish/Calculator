af869a8cd6932abb61dbad1c9be9ddfd6ecb4a55
Evan Plascencia-Rambish <evanp@cpp.edu>
2026-03-03 17:43:14 -0800

    Add more unit tests for add()

--- test_calculator.c
+++ test_calculator.c
@@ -13,10 +13,27 @@ void tearDown(void) {
 // In this example, we do not need anything here.
 }
 void test_add_positive_numbers(void) {
-TEST_ASSERT_EQUAL(5, add(2, 3)); // We expect 2 + 3 to be 5
+    TEST_ASSERT_EQUAL(5, add(2, 3)); // We expect 2 + 3 to be 5
+}
+void test_add_positive_and_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(1, add(5, -4));
+}
+
+void test_add_negative_numbers(void) {
+    TEST_ASSERT_EQUAL(-7, add(-3, -4));
+}
+
+void test_add_zero(void) {
+    TEST_ASSERT_EQUAL(10, add(10, 0));
+    TEST_ASSERT_EQUAL(0, add(0, 0));
 }
 int main(void) {
-UNITY_BEGIN();
-RUN_TEST(test_add_positive_numbers); // Run our test function
-return UNITY_END();
+    UNITY_BEGIN();
+
+    RUN_TEST(test_add_positive_numbers);
+    RUN_TEST(test_add_positive_and_negative_numbers);
+    RUN_TEST(test_add_negative_numbers);
+    RUN_TEST(test_add_zero);
+
+    return UNITY_END();
 }
